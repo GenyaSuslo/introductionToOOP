3446675c1c4ff23e198ea26ca45de48cfb6ea1dd
Book <Book@WIN-8CS3AUJICSU>
2023-06-25 21:49:31 +0300

    reload

--- introduction To OOP/Source.cpp
+++ introduction To OOP/Source.cpp
@@ -19,14 +19,14 @@ public:
 	}
 	void set_x(double x)
 	{
-		this-> x = x;
+		this->x = x;
 	}
 	void set_y(double y)
 	{
-		this-> y = y;
+		this->y = y;
 	}
 	//				Constructor:
-	
+
 	/*Point()
 	{
 		x = y = double();	// type() - это значение по умолчанию для заданного типа
@@ -40,20 +40,20 @@ public:
 		this->y = 0;
 		cout << "1ArgConstructor: \t"<<this<<endl;
 	}*/
-	
-	Point(double x=0,double y=0) //более универсальный
+
+	Point(double x = 0, double y = 0) //более универсальный конструктор по умолчанию
 	{
 		this->x = x;
 		this->y = y;
 		cout << "Constructor:\t\t" << this << endl;
 	}
-	Point(const Point& other)
+	Point(const Point& other)//конструктор копирования
 	{
 		this->x = other.x;
 		this->y = other.y;
 		cout << "CopyConstructor:\t" << this << endl;
 	}
-	~Point()
+	~Point()//деструктор,удаляет объекты после выполнения функции
 	{
 		cout << "Destructor: \t\t" << this << endl;
 	}
@@ -66,6 +66,29 @@ public:
 		return *this;
 
 	}
+	Point& operator++()//префиксный инкремент
+	{
+		x++;
+		y++;
+		return *this;
+	}
+	Point operator++(int)//постфиксный инкремент
+	{
+		Point old = *this;//сохраняем исходное значение объекта
+		x++;
+		y++;
+		return old;
+	}
+
+
+	//арифметические операторы перегружаются за классом!!
+	Point& operator+=(const Point& other)
+	{
+		this->x += other.x;
+		this->y += other.y;
+		return *this;
+	}
+
 
 	//				Methods:
 	double distance(const Point& other)const //константный метод который не изменяет объект для которого вызывается
@@ -80,21 +103,45 @@ public:
 	{
 		cout << "X = " << x << "\t Y = " << y << endl;
 	}
+
 };
 
 double distance(const Point& A, const Point& B) //передаче по константной ссылке,чтобы не изменить значение
 {
-	
+
 	double x_distance = A.get_x() - B.get_x();
 	double y_distance = A.get_y() - B.get_y();
 	return sqrt(x_distance * x_distance + y_distance * y_distance);
 
-}
+};
 
+Point operator+(const Point& left, const Point& right)
+{
+	/*Point result;
+	result.set_x(left.get_x() + right.get_x());
+	result.set_y(left.get_y() + right.get_y());*/
+	/*Point result
+	(
+		left.get_x() + right.get_x(),
+		left.get_y() + right.get_y()
+	);
+	return result;*/
+	return Point
+	(
+		left.get_x() + right.get_x(),
+		left.get_y() + right.get_y()
+	);
+};
+void operator<<(ostream& os, const Point& obj)
+{
+	os << "X= " << obj.get_x() << "\tY= " << obj.get_y();
+}
 
 //#define STRUCT_POINT
 //#define DISTANCE_CHECK
 //#define CONSTRUCTOR_CHEK
+//#define assignment_check
+//#define increment_check
 
 void main()
 {
@@ -134,7 +181,7 @@ void main()
 	cout << "расстояние от точки 'Б' до точки 'А':\t " << distance(B, A) << endl;
 	cout << delimiter << endl;
 #endif // DISTANCE_CHECK
-	
+
 #ifdef CONSTRUCTOR_CHEK
 	Point A;// default constructor
 	A.print();
@@ -149,7 +196,7 @@ void main()
 	E = D;		//CopyAssigment
 	E.print();
 #endif CONSTRUCTOR_CHEK
-
+#ifdef assignment_check
 	int a, b, c;
 	a = b = c = 0;
 	cout << a << "\t" << b << "\t" << c << endl;
@@ -158,4 +205,30 @@ void main()
 	A.print();
 	B.print();
 	C.print();
+#endif assignment_check
+#ifdef increment_chek
+	int a = 2;
+	int b = 3;
+	int c = a + b;
+	cout << " int c = " << c << endl;
+	Point A(2, 3);
+	Point B(7, 8);
+	Point C = A + B;
+	C.print();
+	A++;
+	A.print();
+	/*B++;
+	B.print();*/
+#endif increment_check
+
+	Point A(2, 3);
+	A.print();
+	Point B(7, 8);
+	B.print();
+
+	A += B;
+	A.print();
+	cout << typeid(cout).name() << endl;
+	cout << A;
+
 }
