21148cae4d64bcfe808bf09cb2e01eeb0c4d373f
Евгений Суслопаров <suslegept@yahoo.com>
2023-07-07 15:35:19 +0300

    encoding

--- String/main.cpp
+++ String/main.cpp
@@ -6,6 +6,12 @@ class String
 	int size; //размер указывать лучше в байтах
 	char* str; //адрес строки в динамической памяти
 public:
+
+	const char* get_str()const
+	{
+		return str;
+	}
+
 	//			Constructors
 
 	String(int size = 80)
@@ -14,6 +20,15 @@ public:
 		this->str = new char[size] {};
 		cout << "DefConstructor: \t" << this << endl;
 	}
+	String(const char* str)
+	{
+		this->size = strlen(str) + 1;//strlen возвращает размер в символах, по этому +1,т.к. у нас в байтах
+		this->str = new char[size] {};
+		for (size_t i = 0; str[i]; i++)this->str[i] = str[i];
+		cout << "Constructor:\t" <<this <<endl;
+	
+	}
+
 	~String()
 	{
 		delete[] str;
@@ -28,12 +43,18 @@ public:
 	}
 };
 
+std::ostream& operator<<(std::ostream& os, const String& obj)
+{
+	return os << obj.get_str();
+}
+
 void main()
 {
 	setlocale(LC_ALL, "");
 
 	String str;
 	str.print();
-	
+	String str1 = "Hello";
+	cout << str1 << endl;
 	
 }
